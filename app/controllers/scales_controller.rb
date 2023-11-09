7c94b0702c1d7251eeee9c48398ff042804ce77b
Litsea <litsea52@gmail.com>
2023-11-09 13:59:16 +0900

    add: scalesコントローラーを追加

---
+++ app/controllers/scales_controller.rb
@@ -0,0 +1,95 @@
+class ScalesController < ApplicationController
+  before_action :load_scale_index
+  before_action :validate_tonic, only: :show
+
+  def index
+    scale_list = []
+
+    Constant::ScaleCategory.includes(:scales).find_each do |category|
+      scales = category.scales.map { |scale| generate_scale_hash(scale) }
+      scale_list.push(category: category.name, scales:)
+    end
+
+    render json: scale_list
+  end
+
+  def show
+    tonic = params[:tonic]
+
+    scale = Constant::Scale.includes(scale_tones: %i[accidental interval tone_type]).find_by(path: params[:scale].to_s)
+
+    notes = Constant::ScaleTone.generate_scale_notes(scale.scale_tones, tonic, scale.scale_category_id, scale.path)
+    tones = scale.scale_tones.map.with_index { |tone, idx| generate_tone_hash(tone, notes[idx]) }
+
+    scale_info = {
+      'name' => format_note(tonic) + format_accidental(scale.name),
+      'description' => edit_scale_info_description(scale.description, tones),
+      'tones' => tones
+    }
+
+    render json: scale_info
+  end
+
+  private
+
+  def load_scale_index
+    @scale_index = Constant::Scale.all.index_by(&:id)
+  end
+
+  def validate_tonic
+    render status: :bad_request, json: { error: 'Invalid tonic parameter' } unless TONIC_NOTES.include?(params[:tonic])
+  end
+
+  def generate_scale_hash(scale)
+    {
+      'path' => scale.path,
+      'name' => format_accidental(scale.name),
+      'description' => edit_scale_list_description(scale.description)
+    }
+  end
+
+  def format_accidental(str)
+    str.tr('#b', '♯♭')
+  end
+
+  def edit_scale_list_description(description)
+    edited_description = description.gsub(/\[\d\]/, '')
+
+    edited_description.gsub!(/\{interval:(\d),scale_id:(\d+)\}/) do |_match|
+      related_scale = @scale_index[$2.to_i]
+      related_scale.name
+    end
+
+    format_accidental(edited_description)
+  end
+
+  def generate_tone_hash(tone, note)
+    {
+      'interval' => tone.interval.name,
+      'note' => note,
+      'tone_type' => tone.tone_type.name
+    }
+  end
+
+  def format_note(note)
+    note.length == 1 ? note.upcase : note.slice(0).upcase + SYMBOLS[note.slice(1..-1).to_sym]
+  end
+
+  def edit_scale_info_description(description, tones)
+    edited_description = format_accidental(description).gsub(/\[(\d)\]/) do |_match|
+      format_note(tones[$1.to_i]['note'])
+    end
+
+    edited_description.gsub(/\{interval:(\d),scale_id:(\d+)\}/) do |_match|
+      related_scale_tonic = tones[$1.to_i]['note']
+      related_scale = @scale_index[$2.to_i]
+
+      if TONIC_NOTES.include?(related_scale_tonic)
+        "<a href=\"./#{related_scale.path}?tonic=#{related_scale_tonic}\">" \
+        "#{format_note(related_scale_tonic)}#{format_accidental(related_scale.name)}</a>".html_safe
+      else
+        "#{format_note(related_scale_tonic)}#{format_accidental(related_scale.name)}"
+      end
+    end
+  end
+end
--- app/models/constant/scale_tone.rb
+++ app/models/constant/scale_tone.rb
@@ -2,4 +2,120 @@ class Constant::ScaleTone < ApplicationRecord
   belongs_to :accidental
   belongs_to :interval
   belongs_to :tone_type
+
+  def self.generate_scale_notes(tones, tonic, scale_category_id, path)
+    notes = tones.map { |tone| calc_relative_note(tonic, tone.semitone_cnt, tone.accidental.name) }
+    ref = %w[c d e f g a b]
+    ref = ref.slice(ref.index(tonic[0])..-1) + ref.slice(0, ref.index(tonic[0]))
+
+    case path
+    when 'major-pentatonic', 'minor-pentatonic'
+      ref.delete_if.with_index { |_note, i| path == 'major-pentatonic' ? [3, 6].include?(i) : [1, 5].include?(i) }
+    when 'major-blues', 'minor-blues'
+      ref.delete_if.with_index { |_note, i| path == 'major-blues' ? [3, 6].include?(i) : [1, 5].include?(i) }
+      ref.insert(path == 'major-blues' ? 2 : 3, ref[path == 'major-blues' ? 2 : 3])
+    when 'diminished', 'bebop-dominant'
+      ref.push(ref[6])
+    when 'dominant-diminished'
+      ref.insert(1, ref[1])
+    when 'altered'
+      ref[4] = ref[3]
+      ref[3] = ref[2]
+      ref[2] = ref[1]
+    else
+      return notes unless (1..4).include?(scale_category_id)
+    end
+
+    accidentals = tones.map(&:accidental).map(&:name)
+    adjust_accidentals(notes, accidentals, ref)
+  end
+
+  def self.calc_relative_note(tonic, semitone_cnt, accidental)
+    notes = if %w[c csharp d dsharp e fsharp g gsharp a asharp b].include?(tonic)
+              CHROMATIC_NOTES_SHARP.slice(CHROMATIC_NOTES_SHARP.index(tonic)..-1) + CHROMATIC_NOTES_SHARP.slice(0, CHROMATIC_NOTES_SHARP.index(tonic))
+            elsif %w[dflat eflat f gflat aflat bflat].include?(tonic)
+              CHROMATIC_NOTES_FLAT.slice(CHROMATIC_NOTES_FLAT.index(tonic)..-1) + CHROMATIC_NOTES_FLAT.slice(0, CHROMATIC_NOTES_FLAT.index(tonic))
+            elsif tonic == 'esharp'
+              %w[esharp fsharp g gsharp a asharp b c csharp d dsharp e]
+            elsif tonic == 'bsharp'
+              %w[bsharp csharp d dsharp e f fsharp g gsharp a asharp b]
+            elsif tonic == 'fflat'
+              %w[fflat f gflat g aflat a bflat b c dflat d eflat]
+            else
+              %w[cflat c dflat d eflat e f gflat g aflat a bflat]
+            end
+
+    note = notes[semitone_cnt]
+
+    if accidental == 'sharp'
+      note = if note.include?('sharp')
+               note.gsub('sharp', 'doublesharp')
+             elsif note.include?('flat')
+               note.gsub('flat', 'natural')
+             else
+               "#{note}sharp"
+             end
+    elsif accidental == 'flat'
+      note = if note.include?('flat')
+               note.gsub('flat', 'doubleflat')
+             elsif note.include?('sharp')
+               note.gsub('sharp', 'natural')
+             else
+               "#{note}flat"
+             end
+    end
+
+    note
+  end
+
+  def self.adjust_accidentals(notes, accidentals, ref)
+    adjusted_notes = []
+    notes.zip(accidentals).each_with_index do |(note, accidental), idx|
+      adjusted_notes << (note[0] == ref[idx] ? note : adjust_accidental(note, ref[idx], accidental))
+    end
+
+    adjusted_notes
+  end
+
+  def self.adjust_accidental(note, base_alphabet, accidental)
+    enharmonic_note = handle_enharmonic_note(note)
+    chromatic_notes_for_calc = CHROMATIC_NOTES_SHARP.slice(CHROMATIC_NOTES_SHARP.index(enharmonic_note)..-1) +
+                               CHROMATIC_NOTES_SHARP.slice(0, CHROMATIC_NOTES_SHARP.index(enharmonic_note))
+    chromatic_notes_for_calc.unshift(*chromatic_notes_for_calc.pop(2))
+
+    semitone_distance = chromatic_notes_for_calc.index(base_alphabet) - chromatic_notes_for_calc.index(enharmonic_note)
+
+    case semitone_distance
+    when 0
+      accidental.present? ? "#{base_alphabet}natural" : base_alphabet
+    when -1
+      "#{base_alphabet}sharp"
+    when -2
+      "#{base_alphabet}doublesharp"
+    when 1
+      "#{base_alphabet}flat"
+    when 2
+      "#{base_alphabet}doubleflat"
+    else
+      note
+    end
+  end
+
+  def self.handle_enharmonic_note(note)
+    if note.include?('doublesharp')
+      note[0] == 'b' ? 'csharp' : CHROMATIC_NOTES_SHARP[CHROMATIC_NOTES_SHARP.index(note[0]) + 2]
+    elsif note.include?('doubleflat')
+      note[0] == 'c' ? 'asharp' : CHROMATIC_NOTES_SHARP[CHROMATIC_NOTES_SHARP.index(note[0]) - 2]
+    elsif note.include?('flat')
+      note[0] == 'c' ? 'b' : CHROMATIC_NOTES_SHARP[CHROMATIC_NOTES_SHARP.index(note[0]) - 1]
+    elsif note == 'esharp'
+      'f'
+    elsif note == 'bsharp'
+      'c'
+    elsif note.include?('natural')
+      note[0]
+    else
+      note
+    end
+  end
 end

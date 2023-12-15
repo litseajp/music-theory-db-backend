6fe79ffb41a968a5d70505c3c1e939bb1f23a355
Litsea <litsea52@gmail.com>
2023-12-15 23:49:33 +0900

    refactor: スケールの算出ロジックを修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_11_05_143136) do
-  create_table "constant_accidentals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-  end
-
   create_table "constant_intervals", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
   end
@@ -25,11 +21,10 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_05_143136) do
 
   create_table "constant_scale_tones", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "scale_id"
-    t.integer "semitone_cnt", null: false
-    t.bigint "accidental_id"
+    t.integer "semitone_distance", null: false
+    t.integer "alphabet_distance"
     t.bigint "interval_id"
     t.bigint "tone_type_id"
-    t.index ["accidental_id"], name: "index_constant_scale_tones_on_accidental_id"
     t.index ["interval_id"], name: "index_constant_scale_tones_on_interval_id"
     t.index ["scale_id"], name: "index_constant_scale_tones_on_scale_id"
     t.index ["tone_type_id"], name: "index_constant_scale_tones_on_tone_type_id"
@@ -47,7 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_05_143136) do
     t.string "name", null: false
   end
 
-  add_foreign_key "constant_scale_tones", "constant_accidentals", column: "accidental_id"
   add_foreign_key "constant_scale_tones", "constant_intervals", column: "interval_id"
   add_foreign_key "constant_scale_tones", "constant_scales", column: "scale_id"
   add_foreign_key "constant_scale_tones", "constant_tone_types", column: "tone_type_id"
